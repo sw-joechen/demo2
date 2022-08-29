7d4b37ed4a92968ba2c90e5085e51d9c396d95a7
swjoechen <swjoechen@gmail.com>
2022-08-29 17:52:26 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string;
+}>();
+
+const emits = defineEmits<{
+  (event: "Demo", id: number): void;
+}>();
+
+const ClickHandler = () => {
+  emits("Demo", 789);
+};
+</script>
+
+<template>
+  <button @click="ClickHandler">click me</button>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
